cb78144834c87cc1077bb33b776880be5bfbe91c
nitsujz <150884051+nitsujz@users.noreply.github.com>
2024-05-09 14:10:50 -0600

    added based code for major project

--- proposal.md
+++ proposal.md
@@ -7,12 +7,11 @@
 - Randomly spawn blocks and move down automatically
 - Blocks have different shapes and colors
 - Be able to move the blocks side to side
-- Be able to rotate the blocks
 - Detect collision to make block stop
-- Make a score tracker
-- Endgame
 
 ## Nice to have list
+- Make a score tracker
+- Be able to rotate the blocks
 - Start screen, end screen
 - Music
 - Sound effects
--- sketch.js
+++ sketch.js
@@ -1,13 +1,155 @@
-// Tetris
-// Justin Nguyen
+//Grid assignment
+//Justin Nguyen
 
+//make grid and blocks
+const grid = 
+[[0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+];
+
+const blocks = {
+  I: [
+    [1, 1, 1, 1]
+  ],
+
+  L: [
+    [1, 0],
+    [1, 0],
+    [1, 1],
+  ],
+
+  U: [
+    [0, 1],
+    [0, 1],
+    [1, 1],
+  ],
+
+  O: [
+    [1, 1],
+    [1, 1],
+  ],
+
+  S: [
+    [1, 1, 0],
+    [0, 1, 1],
+  ],
+
+  T: [
+    [1, 1, 1],
+    [0, 1, 0],
+  ],
+
+  Z: [
+    [0, 1, 1],
+    [1, 1, 0],
+  ],
+};
+
+let currentBlock;
+let cellSize;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  cellSize = height / grid.length;
+  let block = new Block();
+  currentBlock = generateRandomBlock();
 }
 
 function draw() {
   background(220);
+  showGrid();
   
-  square(mouseX, mouseY, 100);
+  if (currentBlock) {
+    currentBlock.update();
+    currentBlock.show();
+    currentBlock.addAnotherBlock();
+  }
+}
+
+function keyPressed() {
+  if (currentBlock) {
+    if (key === "w") {
+      currentBlock.moveLeft();
+    }
+    else if (key === "d") {
+      currentBlock.moveRight();
+    }
+    else if (key === "s") {
+      currentBlock.moveDown();
+    }
+  }
+}
+
+//use to create random block
+function generateRandomBlock() {
+  const blockTypes = Object.keys(blocks);
+  const randomType = blockTypes[Math.floor(Math.random() * blockTypes.length)];
+  const randomShape = blocks[randomType];
+  return new Block(randomShape);
+}
+
+//show grid
+function showGrid() {
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
+      fill(grid[y][x] === 1 ? "black" : "white");
+      square(x * cellSize, y * cellSize, cellSize);
+    }
+  }
+}
+
+//make block
+class Block {
+  constructor(shape, color) {
+    this.shape = shape;
+    this.x = Math.floor(cellSize / 2) - Math.floor(shape[0].length / 2); 
+    this.y = 0; 
+    this.color = color;
+  }
+
+  show() {
+    for (let y = 0; y < this.shape.length; y++) {
+      for (let x = 0; x < this.shape[y].length; x++) {
+        if (this.shape[y][x]) {
+          fill("white");
+          square((this.x + x) * cellSize, (this.y + y) * cellSize, cellSize);
+        }
+      }
+    }
+  }
+
+  update() {
+    this.moveDown();
+    this.color = color(random(255), random(255), random(255));
+  }
+
+  addAnotherBlock() {
+    for (let y = 0; y < this.shape.length; y++) {
+      for (let x = 0; x < this.shape[y].length; x++) {
+        if (this.shape[y][x]) {
+          grid[this.y + y][this.x + x] = 1;
+        }
+      }
+    }
+  }
+
+  moveDown() {
+    this.y++;
+  }
+  moveLeft() {
+    this.x--;
+  }
+  moveRight() {
+    this.x++;
+  }
 }
